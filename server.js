1a9cbb3afb9ccdfc6a28e91fa97d6c1b73b0922c
alexmcbex <alessiobelli94@gmail.com>
2024-10-02 10:15:42 -0400

    added pokemon of the day in index

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PokemonRouter = require('./controllers/pokemonRoute')
 const PokedexRouter = require('./controllers/pokedexRoute')
 const ProfileRouter = require('./controllers/profileRoute')
 const Pokemon = require('./models/pokemon')
+const axios = require("axios")
 // SEE MORE DEPENDENCIES IN ./utils/middleware.js
 // user and resource routes linked in ./utils/middleware.js
 
@@ -38,15 +39,28 @@ app.get('/health', (req, res) => {
 
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     const { username, userId, loggedIn } = req.session
-	res.render('index.liquid', { loggedIn, username, userId })
+	const today = new Date().toISOString().split('T')[0]
+	const getRandomId = () =>{
+		const today = new Date().toISOString().split('T')[0]
+		let hash = 0
+		for (let i =0 ; i < today.length; i++){
+			hash = today.charCodeAt(i) + ((hash <<5) - hash)
+			hash = hash & hash
+		}
+
+		return Math.abs(hash % 1025) +1
+	}
+	randomId = getRandomId()
+	const randomPokemon = await axios(`${process.env.POKEAPI_URL}/pokemon/${randomId}`)
+	res.render('index.liquid', { loggedIn, username, userId, randomPokemon, today, randomId,  })
 })
 
 app.get('/error', (req, res) => {
 	const error = req.query.error || 'This Page Does Not Exist'
     const { username, loggedIn, userId } = req.session
-	res.render('error.liquid', { error, username, loggedIn, userId })
+	res.render('error.liquid', { error, username, loggedIn, userId }) 
 })
 
 // if page is not found, send to error page
